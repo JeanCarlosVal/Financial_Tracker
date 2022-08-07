0b080affbc82ee7f7fa97a2b9bb0f25db55dfe5c
JeanCarlosVal <Jean_Valenzuela2021@outlook.com>
2022-08-07 18:50:06 -0400

    Bootstrap 5.2 update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace Personal_Income.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Home()
         {
             return View();
         }
