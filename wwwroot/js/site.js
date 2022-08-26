30f1771795fc4075021d3c5156644ca9f4838bd0
JeanCarlosVal <Jean_Valenzuela2021@outlook.com>
2022-08-25 20:53:07 -0400

    user sidebar in progress

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -37,3 +37,14 @@ $(".togglePassword").click(function (e) {
         $(this).parent().parent().find(".password").attr("type", "password");
     }
 });
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
